9fe42197f6b1126fe7f933e5cd77e440a8a8526f
Vinay Kumar <12234745+imflash217@users.noreply.github.com>
2025-01-28 01:34:19 +0530

    query route design and bugfixes

--- flashrag/hyperparams.py
+++ flashrag/hyperparams.py
@@ -1,4 +1,7 @@
-ml_models = {}
+llm = None
 db_name = {}
 model_args = {}
-vector_db_model_name = ""
+vector_db_embeddings_model_name = ""
+vector_db_collection_name_default = "flashrag_collection_default"
+vector_db_num_responses = 2
+vector_db_collections_fname = "COLLECTIONS.txt"
--- flashrag/main.py
+++ flashrag/main.py
@@ -12,8 +12,8 @@ async def app_lifespan(app: FastAPI):
     # 1. load the LLM model
     llm = utils.get_llm()
 
-    # 2. save the llm model for application to use
-    hp.ml_models["llm"] = llm
+    # 2. save the llm for application to use
+    hp.llm = llm
 
     # run application
     yield
@@ -39,14 +39,18 @@ def index():
 
 @rag_app.get("/init_llm")
 def init_llm():
+    # TODO!
     kwargs = hp.model_args
     llm = utils.get_llm(**kwargs)
-    hp.ml_models["llm"] = llm
+    hp.llm = llm
     return {"message": "LLM initialized", "LLM init params": kwargs}
 
 
 @rag_app.post("/upload")
-def upload_docs(docs: list[UploadFile] = File(...)):
+def upload_docs(
+    docs: list[UploadFile] = File(...),
+    collection_name: str = hp.vector_db_collection_name_default,
+):
     docs_metadata = {}
     for doc in docs:
         # step-1: load/read every uploaded document
@@ -70,16 +74,43 @@ def upload_docs(docs: list[UploadFile] = File(...)):
         # TODO!
 
     return {
-        "message": f"Successfully uploaded & stored {len(docs)} files",
+        "message": f"Successfully uploaded & stored {len(docs)} files to [{collection_name}] collection",
+        "collection_name": collection_name,
         "docs_metadata": docs_metadata,
     }
 
 
 @rag_app.get("/query")
-def query():
-    pass
+def query(
+    query: str,
+    num_responses: int = hp.vector_db_num_responses,
+    collection_name: str = hp.vector_db_collection_name_default,
+):
+    # step-1: get list of all collections
+    collections = []
+    try:
+        with open(hp.vector_db_collections_fname, "r") as f:
+            collections = f.read().split("\n")[:-1]
+    except Exception:
+        return {"message": "No collections found. Upload some documents first."}
+
+    # step-2: if the queried collection is not found RETURN
+    if collection_name not in collections:
+        return {
+            "message": f"The collection name = {collection_name} NOT found in the available collections list",
+            "collection_name": collection_name,
+        }
+
+    # step-3: if the queried collection is available:
+    # step-3: - load the collection
+    # step-3: - retrieve the matching vector items to the query
+    # step-3: - create a prompt using the query & retrieved items
+    # TODO!
+
+    # step-4: send prompt to the LLM & return the response
+    # TODO!
 
 
 # necessary to run the application via uvicorn
 if __name__ == "__main__":
-    uvicorn.run(app="main:rag_app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app="main:rag_app", reload=True)
